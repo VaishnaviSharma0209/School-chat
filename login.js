30bf49ccebca9c02e14482e4ed23823f1f50aaf9
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2021-01-27 20:39:52 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+function login(){
+    password=document.getElementById("password").value;
+    user_name=document.getElementById("user").value;
+    password1=localStorage.getItem("user_name");
+    user1=localStorage.getItem("passsword");
+    if (user1==user_name&&password==password1){
+        console.log("Hi");
+        window.location="Main_page.html";
+        localStorage.setItem("user_name",user_name);
+    }
+    else {
+        document.getElementById("error").innerHTML="Incorrect password or username or register and try again";
+    }
+}
+function register(){
+    window.location="index.html";
+}
--- register.js
+++ register.js
@@ -1,3 +1,23 @@
 function login(){
     window.location="login.html";
 }
+function register(){
+    pass1=document.getElementById("password").value;
+    pass2=document.getElementById("con_password").value;
+    user=document.getElementById("user").value;
+    var j=0;
+    if (pass1==pass2){
+        j=j+1;
+    }
+    else{
+        document.getElementById("error").innerHTML="Please write the same password";
+    }
+    if (user==""){
+        document.getElementById("error").innerHTML="Please type username";
+    }
+    if (j==1&&user!=""){
+        localStorage.setItem("user_name",user);
+        localStorage.setItem("password",pass1);
+        window.location="login.html";
+    }
+}
