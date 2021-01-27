b4bcd98f47c975e4ab83bcc0b830f27e58d8e439
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2021-01-27 21:10:04 +0530

    Add files via upload

---
+++ Main_page.js
@@ -0,0 +1,40 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDHsGIb1Y2K50sItxijWJyIQtnCzL0AY9s",
+    authDomain: "school-chat-87efe.firebaseapp.com",
+    projectId: "school-chat-87efe",
+    storageBucket: "school-chat-87efe.appspot.com",
+    databaseURL: "https://school-chat-87efe-default-rtdb.firebaseio.com/",
+    messagingSenderId: "517874830218",
+    appId: "1:517874830218:web:885c42149917be46a1d673"
+  }
+  firebase.initializeApp(firebaseConfig);
+    user_name=localStorage.getItem("user_name")
+    document.getElementById("welcome").innerHTML="Welcome " + user_name + "!";
+    function Addroom(){
+          room_name=document.getElementById("room_name").value;
+          firebase.database().ref("/").child(room_name).update({
+                purpose: "Add room name"
+          });
+          localStorage.setItem("Room Name",room_name);
+          //window.location="Room.html";
+    }
+
+    function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+        Room_names = childKey;
+      //Start code
+      console.log("Room-"+Room_names);
+      row="<div class='room_name' id="+Room_names+" onclick='redirct(this.id)'>#"+Room_names+" </div><hr>";
+      document.getElementById("output").innerHTML+=row;
+      //End code
+      });});}
+getData();
+function redirct(name){
+      console.log(name);
+      localStorage.setItem("Room Name",name);
+     // window.location="Room.html";
+}
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="login.html";
+}
--- login.js
+++ login.js
@@ -3,9 +3,10 @@ function login(){
     user_name=document.getElementById("user").value;
     password1=localStorage.getItem("user_name");
     user1=localStorage.getItem("passsword");
-    if (user1==user_name&&password==password1){
+    k=0;
+    if (user1==user_name && password==password1){
         console.log("Hi");
-        window.location="Main_page.html";
+        window.location="index.html";
         localStorage.setItem("user_name",user_name);
     }
     else {
